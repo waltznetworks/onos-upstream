5c10813c8cbb747a1ac9467b52df46a544205b0b
Hans Yu <chinghanyu@ntu.edu.tw>
2016-08-05 15:43:57 -0700

    NC-486: Add empty functions for port stats polling
    Set up a timer in NetconfDeviceProvider and periodically poll port statistics from devices.
    Function bodies will come later.

--- providers/netconf/device/src/main/java/org/onosproject/provider/netconf/device/impl/NetconfDeviceProvider.java
+++ providers/netconf/device/src/main/java/org/onosproject/provider/netconf/device/impl/NetconfDeviceProvider.java
@@ -23,6 +23,7 @@ import org.apache.felix.scr.annotations.Deactivate;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.ReferenceCardinality;
 import org.onlab.packet.ChassisId;
+import org.onlab.util.SharedScheduledExecutors;
 import org.onosproject.cluster.ClusterService;
 import org.onosproject.cluster.NodeId;
 import org.onosproject.core.ApplicationId;
@@ -73,6 +74,7 @@ import java.util.concurrent.TimeUnit;
 
 import static java.util.concurrent.Executors.newScheduledThreadPool;
 import static org.onlab.util.Tools.groupedThreads;
+import static org.onosproject.net.Device.Type.ROUTER;
 import static org.onosproject.net.config.basics.SubjectFactories.APP_SUBJECT_FACTORY;
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -151,6 +153,10 @@ public class NetconfDeviceProvider extends AbstractProvider
     private ApplicationId appId;
     private boolean active;
 
+    private static final int POLL_PERIOD = 5_000; // milliseconds
+    private final ScheduledExecutorService scheduledExecutorService = SharedScheduledExecutors.getPoolThreadExecutor();
+    private ScheduledFuture<?> poller;
+
 
     @Activate
     public void activate() {
@@ -164,6 +170,8 @@ public class NetconfDeviceProvider extends AbstractProvider
         executor.execute(NetconfDeviceProvider.this::connectDevices);
         localNodeId = clusterService.getLocalNode().id();
         scheduledTask = schedulePolling();
+        /* Poll devices every 1000 milliseconds */
+        poller = scheduledExecutorService.scheduleAtFixedRate(this::pollDevices, 1_000, POLL_PERIOD, TimeUnit.MILLISECONDS);
         log.info("Started");
     }
 
@@ -182,6 +190,9 @@ public class NetconfDeviceProvider extends AbstractProvider
         providerService = null;
         cfgService.unregisterConfigFactory(factory);
         scheduledTask.cancel(true);
+        if (poller != null) {
+            poller.cancel(false);
+        }
         executor.shutdown();
         log.info("Stopped");
     }
@@ -278,6 +289,25 @@ public class NetconfDeviceProvider extends AbstractProvider
     public void changePortState(DeviceId deviceId, PortNumber portNumber,
                                 boolean enable) {
         // TODO if required
+    }
+
+    private void pollDevices() {
+        for (Device device: deviceService.getAvailableDevices(ROUTER)) {
+            if (mastershipService.isLocalMaster(device.id())) {
+                executor.execute(() -> pollingTask(device.id()));
+            }
+        }
+    }
+
+    private void pollingTask(DeviceId deviceId) {
+        log.debug("Polling device {}...", deviceId);
+        if (isReachable(deviceId)) {
+            log.debug("Netconf device {} is reachable, updating ports and stats", deviceId);
+            //updatePortsAndStats(deviceId);
+        } else {
+            log.debug("Netconf device {} is unreachable, disconnecting", deviceId);
+            //disconnectDevice(deviceId);
+        }
     }
 
     private class InnerNetconfDeviceListener implements NetconfDeviceListener {
