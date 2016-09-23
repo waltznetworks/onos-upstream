08cbd2fb3fb47a19fc3599ce40ced8072c7bb885
Hans Yu <chinghanyu@ntu.edu.tw>
2016-09-23 10:24:09 -0700

    NC-386: Add subroutines for NETCONF message building
    Add subroutines that help build headers and footers of NETCONF
    messages.

--- drivers/cisco/src/main/java/org/onosproject/drivers/cisco/ConfigSetterCsr1000vImpl.java
+++ drivers/cisco/src/main/java/org/onosproject/drivers/cisco/ConfigSetterCsr1000vImpl.java
@@ -25,15 +25,39 @@ public class ConfigSetterCsr1000vImpl extends AbstractHandlerBehaviour implement
     public String setConfiguration(String request) {
         NetconfController controller = checkNotNull(handler().get(NetconfController.class));
         NetconfSession session = controller.getDevicesMap().get(handler().data().deviceId()).getSession();
-        String reply;
+        String reply, requestNetConfString = new StringBuilder(buildHeadPattern())
+                .append(request).append(buildTailPattern()).toString();
+        log.info("Sending out netconf request {}", requestNetConfString);
 
         try {
-            reply = session.get(request);
+            reply = session.get(requestNetConfString);
             log.debug("Device {} replies {}", handler().data().deviceId(), reply.replaceAll("\r", ""));
         } catch (IOException e) {
             throw new RuntimeException(new NetconfException("Failed to set configuration via netconf", e));
         }
 
         return reply;
+    }
+
+    private String buildHeadPattern() {
+        StringBuilder pattern = new StringBuilder("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+        pattern.append("<rpc>");
+        pattern.append("<edit-config>");
+        pattern.append("<target>");
+        pattern.append("<running/>");
+        pattern.append("</target>");
+        pattern.append("<config>");
+        pattern.append("<cli-config-data>");
+
+        return pattern.toString();
+    }
+
+    private String buildTailPattern() {
+        StringBuilder pattern = new StringBuilder("</cli-config-data>");
+        pattern.append("</config>");
+        pattern.append("</edit-config>");
+        pattern.append("</rpc>");
+
+        return pattern.toString();
     }
 }
