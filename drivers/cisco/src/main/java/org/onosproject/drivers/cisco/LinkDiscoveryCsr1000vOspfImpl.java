949b7524a5de8214e5b0f962d2d0a3c747aa9610
Hans Yu <chinghanyu@ntu.edu.tw>
2016-10-02 21:59:39 -0700

    Implement request builder and empty reply parser

--- drivers/cisco/src/main/java/org/onosproject/drivers/cisco/LinkDiscoveryCsr1000vOspfImpl.java
+++ drivers/cisco/src/main/java/org/onosproject/drivers/cisco/LinkDiscoveryCsr1000vOspfImpl.java
@@ -1,13 +1,21 @@
 package org.onosproject.drivers.cisco;
 
+import org.apache.commons.configuration.HierarchicalConfiguration;
+import org.onosproject.drivers.utilities.XmlConfigParser;
 import org.onosproject.net.behaviour.LinkDiscovery;
 import org.onosproject.net.driver.AbstractHandlerBehaviour;
 import org.onosproject.net.link.LinkDescription;
+import org.onosproject.netconf.NetconfController;
+import org.onosproject.netconf.NetconfException;
+import org.onosproject.netconf.NetconfSession;
 import org.slf4j.Logger;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
+import static com.google.common.base.Preconditions.checkNotNull;
 import static org.slf4j.LoggerFactory.getLogger;
 
 /**
@@ -20,6 +28,56 @@ public class LinkDiscoveryCsr1000vOspfImpl extends AbstractHandlerBehaviour
 
     @Override
     public Set<LinkDescription> getLinks() {
+        NetconfController controller = checkNotNull(handler().get(NetconfController.class));
+        NetconfSession session = controller.getDevicesMap().get(handler().data().deviceId()).getSession();
+        String reply;
+        try {
+            reply = session.get(getOspfLinksRequestBuilder());
+            log.debug("Device {} replies {}", handler().data().deviceId(), reply.replaceAll("\r", ""));
+        } catch (IOException e) {
+            throw new RuntimeException(new NetconfException("Failed to retrieve configuration.", e));
+        }
+
+        Set<LinkDescription> links =
+                parseCsr1000vOspfLinks(XmlConfigParser.loadXml(new ByteArrayInputStream(reply.getBytes())));
+
+        return links;
+    }
+
+    /**
+     * Builds a request crafted to get the configuration required to create link
+     * descriptions for the device.
+     * @return The request string.
+     */
+    private String getOspfLinksRequestBuilder() {
+        StringBuilder rpc = new StringBuilder("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+        //Message ID is injected later.
+        rpc.append("<rpc xmlns=\"urn:ietf:params:xml:ns:netconf:base:1.0\">");
+        rpc.append("<get>");
+        rpc.append("<filter>");
+        rpc.append("<config-format-text-cmd>");
+        /* Use regular expression to extract only name of the interfaces from running-config */
+        rpc.append("<text-filter-spec> | include interface </text-filter-spec>");
+        rpc.append("</config-format-text-cmd>");
+        rpc.append("<oper-data-format-text-block>");
+        /* Use regular expression to extract status of the interfaces
+         * Note that there is an space after 'packet'
+         */
+        rpc.append("<exec>show ip ospf neighbor | include Ethernet</exec>");
+        rpc.append("</oper-data-format-text-block>");
+        rpc.append("</filter>");
+        rpc.append("</get>");
+        rpc.append("</rpc>");
+
+        return rpc.toString();
+    }
+
+    /**
+     * Parses a configuration and returns a set of link descriptions for Cisco CSR1000v.
+     * @param cfg a hierarchical configuration but might not in pure XML format
+     * @return a set of link descriptions
+     */
+    private Set<LinkDescription> parseCsr1000vOspfLinks(HierarchicalConfiguration cfg) {
         Set<LinkDescription> links = new HashSet<>();
 
         return links;
