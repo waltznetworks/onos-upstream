99d419b4b52e5d456bea19a095118647539b9bb2
Hans Yu <chinghanyu@ntu.edu.tw>
2016-10-25 14:52:09 -0700

    Add the reply parsing function

--- drivers/cisco/src/main/java/org/onosproject/drivers/cisco/LinkDiscoveryCsr1000vOspfImpl.java
+++ drivers/cisco/src/main/java/org/onosproject/drivers/cisco/LinkDiscoveryCsr1000vOspfImpl.java
@@ -2,8 +2,13 @@ package org.onosproject.drivers.cisco;
 
 import org.apache.commons.configuration.HierarchicalConfiguration;
 import org.onosproject.drivers.utilities.XmlConfigParser;
+import org.onosproject.net.ConnectPoint;
+import org.onosproject.net.DeviceId;
+import org.onosproject.net.Link;
+import org.onosproject.net.PortNumber;
 import org.onosproject.net.behaviour.LinkDiscovery;
 import org.onosproject.net.driver.AbstractHandlerBehaviour;
+import org.onosproject.net.link.DefaultLinkDescription;
 import org.onosproject.net.link.LinkDescription;
 import org.onosproject.netconf.NetconfController;
 import org.onosproject.netconf.NetconfException;
@@ -80,9 +85,46 @@ public class LinkDiscoveryCsr1000vOspfImpl extends AbstractHandlerBehaviour
      */
     private Set<LinkDescription> parseCsr1000vOspfLinks(HierarchicalConfiguration cfg) {
         Set<LinkDescription> links = new HashSet<>();
+        DeviceId localDevice = handler().data().deviceId();
+        DeviceId remoteDevice;
+        PortNumber localPortNumber;
+        PortNumber remotePortNumber;
+        ConnectPoint local;
+        ConnectPoint remote;
+
         List<Object> ospfLinkInfo = cfg.getList("data.cli-oper-data-block.item.response");
+        String[] array;
         for (int i = 0; i < ospfLinkInfo.size(); i++) {
             log.info("OSPF neighbor: {}", ospfLinkInfo.get(i).toString());
+            /* Example string:
+             *   192.168.0.3       1   DOWN            00:00:00    10.100.2.3      GigabitEthernet3
+             *   192.168.0.4       1   FULL/DR         00:00:31    10.100.4.4      GigabitEthernet4
+             *
+             * The first column is the router ID, which Cisco by default use the addresses set on GigabitEthernet1;
+             * the second column is the priority and we don't use it for link discovery; the third one is the status
+             * of the link. As long as it contains "FULL", we can safely assume there is bi-directional full-speed
+             * link; the fourth columns is the IP address set on the interface on the neighbor, we will need to use
+             * this to find out port number later; the fifth column is the local interface of the link and we should
+             * convert it to local port number. Refer to:
+             * http://www.cisco.com/c/en/us/support/docs/ip/open-shortest-path-first-ospf/13688-16.html
+             * for more detail.
+             */
+            array = ospfLinkInfo.get(i).toString().split(" +");                 // split with multiple spaces
+            if (!array[2].contains("FULL") || array.length != 6) {
+                continue;   // skip if there is no bi-directional link
+            }
+
+            remoteDevice = DeviceId.deviceId("netconf:" + array[0] + ":22");    // Cisco devices use SSHv2 (port 22)
+            // Fixme: this works for devices connected through NETCONF on port 22 and we should find a better solution
+            remotePortNumber = PortNumber.ANY;                                  // Mark ANY here
+
+
+            localPortNumber = PortNumber.portNumber(Long.parseLong(array[5].replace("GigabitEthernet", "")));
+
+            local = new ConnectPoint(localDevice, localPortNumber);
+            remote = new ConnectPoint(remoteDevice, remotePortNumber);
+
+            links.add(new DefaultLinkDescription(local, remote, Link.Type.DIRECT, true));
         }
 
         return links;
